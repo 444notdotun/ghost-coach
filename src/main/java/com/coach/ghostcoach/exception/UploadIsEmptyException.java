07659d668c2ccb5e57c3f90be5ca944bac630e18
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-06-11 02:48:04 -0700

    add Maven wrapper and new custom exceptions

---
+++ src/main/java/com/coach/ghostcoach/exception/UploadIsEmptyException.java
@@ -0,0 +1,7 @@
+package com.coach.ghostcoach.exception;
+
+public class UploadIsEmptyException extends GhostCoachException {
+    public UploadIsEmptyException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/coach/ghostcoach/exception/UploadSizeException.java
@@ -0,0 +1,7 @@
+package com.coach.ghostcoach.exception;
+
+public class UploadSizeException extends GhostCoachException {
+    public UploadSizeException(String s) {
+        super(s);
+    }
+}
