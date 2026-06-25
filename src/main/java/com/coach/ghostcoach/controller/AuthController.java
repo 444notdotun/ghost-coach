721883338838364e9e53a36f4e47402e91b33681
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-06-25 09:20:00 -0700

    feat: enable request body validation in AuthController

--- src/main/java/com/coach/ghostcoach/controller/AuthController.java
+++ src/main/java/com/coach/ghostcoach/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.coach.ghostcoach.dtos.request.RegisterRequest;
 import com.coach.ghostcoach.dtos.response.ApiResponse;
 import com.coach.ghostcoach.dtos.response.AuthResponse;
 import com.coach.ghostcoach.service.AuthImplementation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +18,12 @@ public class AuthController {
     private AuthImplementation authImplementation;
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<?>> register(@RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<ApiResponse<?>> register(@Valid @RequestBody RegisterRequest registerRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(authImplementation.register(registerRequest)));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<?>> login(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<ApiResponse<?>> login(@Valid @RequestBody LoginRequest loginRequest){
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(authImplementation.login(loginRequest)));
     }
 }
