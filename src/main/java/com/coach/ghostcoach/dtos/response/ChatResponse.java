90b71ffca787f0c50c7214b6df302963c721ded4
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-06-25 09:19:49 -0700

    feat: include non-null values only in ChatResponse

--- src/main/java/com/coach/ghostcoach/dtos/response/ChatResponse.java
+++ src/main/java/com/coach/ghostcoach/dtos/response/ChatResponse.java
@@ -1,12 +1,15 @@
 package com.coach.ghostcoach.dtos.response;
 
 import com.coach.ghostcoach.data.model.Sender;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatResponse {
     private String content;
     private Sender sender;
